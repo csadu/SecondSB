e2495cff9fea2069791ea22f9d610d58d56372c9
saduchanti <lchantisadu@gmail.com>
2023-08-19 21:21:28 +0530

    first commit

---
+++ src/main/java/com/example/SecondSpringboot/SecondSpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.example.SecondSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondSpringbootApplication.class, args);
+		System.out.print("This is Second Springboot Application... Running successfullly...");
+	}
+
+}
---
+++ src/test/java/com/example/SecondSpringboot/SecondSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SecondSpringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
